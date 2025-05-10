318189a43b834637edc30055dfbd5b7e8961ddec
LCPallares <poncho6257@gmail.com>
2025-05-10 16:21:13 -0500

    agregado headers

--- backend/app/scrapers/ggdeals.py
+++ backend/app/scrapers/ggdeals.py
@@ -4,7 +4,12 @@ import requests
 from typing import Dict, List, Optional
 
 def make_soup(url: str) -> BeautifulSoup:
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive'
+    }
     response = requests.get(url, headers=headers, timeout=30)
     response.raise_for_status()  # Lanza error si hay problemas HTTP
     return BeautifulSoup(response.content, "html.parser")
